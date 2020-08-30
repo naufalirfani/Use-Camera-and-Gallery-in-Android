3c0215ac3c481cdec085b740bb4aaf8ac1f2f58d
naufalirfani <naufal.irfani3@gmail.com>
2020-08-30 10:46:27 +0700

    first commit

---
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -0,0 +1,148 @@
+package com.example.cameraapp
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_camera.setOnClickListener {
+            dispatchTakePictureIntent()
+            btn_camera.supportBackgroundTintList = ContextCompat.getColorStateList(this, R.color.colorAccent);
+        }
+    }
+
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        val options =
+            arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("Add Photo!")
+        builder.setItems(options) { dialog, item ->
+            if (options[item].equals("Take Photo")) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                val f = File(
+                    Environment.getExternalStorageDirectory(),
+                    "temp.jpg"
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f))
+                startActivityForResult(intent, 1)
+                galleryAddPic()
+            } else if (options[item].equals("Choose from Gallery")) {
+                val pickPhoto = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
+                startActivityForResult(pickPhoto, 0)
+            } else if (options[item].equals("Cancel")) {
+                dialog.dismiss()
+            }
+        }
+        builder.show()
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            // Ensure that there's a camera activity to handle the intent
+//            takePictureIntent.resolveActivity(packageManager)?.also {
+//                // Create the File where the photo should go
+//                val photoFile: File? = try {
+//                    createImageFile()
+//                } catch (ex: IOException) {
+//                    // Error occurred while creating the File
+//                    null
+//                }
+//                // Continue only if the File was successfully created
+//                photoFile?.also {
+//                    val photoURI: Uri = FileProvider.getUriForFile(
+//                        this,
+//                        "com.example.cameraapp.fileprovider",
+//                        it
+//                    )
+//                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+//                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+//                }
+//            }
+//        }
+    }
+
+    lateinit var currentPhotoPath: String
+
+    @SuppressLint("SimpleDateFormat")
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
+
+//    @SuppressLint("MissingSuperCall")
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+//            val imgFile = File(currentPhotoPath)
+//
+//            if (imgFile.exists()) {
+//                val myBitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
+//                click_image.setImageBitmap(myBitmap)
+//            }
+//        }
+//    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imgFile = File(currentPhotoPath)
+        when (requestCode) {
+            2 -> if (resultCode == Activity.RESULT_OK) {
+                val imageBitmap = data?.extras?.get("data") as Bitmap
+                click_image.setImageBitmap(imageBitmap)
+            }
+            3 -> if (resultCode == Activity.RESULT_OK) {
+                val selectedImage = data?.data
+                click_image.setImageURI(selectedImage)
+            }
+            1 -> if (resultCode == Activity.RESULT_OK) {
+                val myBitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
+                click_image.setImageBitmap(myBitmap)
+            }
+        }
+    }
+}
